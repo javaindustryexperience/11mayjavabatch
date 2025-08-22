e8bef7103b3472f331e630d7444bf98db77b5b66
Jeevan <javaindustryexperience@gmail.com>
2025-08-22 20:55:15 +0530

    added addition number code

--- DemoMayProject/src/com/sample/Example.java
+++ DemoMayProject/src/com/sample/Example.java
@@ -5,5 +5,9 @@ public class Example {
 	public static void main(String[] args) {
 		
 		System.out.println("example class added here");
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("C contain>>"+c);
 	}
 }
