386bf08169252bf471c350296f8ed665d5750ad2
Jeevan <javaindustryexperience@gmail.com>
2025-08-22 21:35:36 +0530

    arraylist code added here

---
+++ DemoMayProject/src/com/sample/Sample.java
@@ -0,0 +1,15 @@
+package com.sample;
+
+import java.util.ArrayList;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("bangalore");
+		System.out.println(al);
+	}
+}
